f9f553725a2c7c544659b91d52dcad91cde5833d
lastaur0ch <joelxjohnson@gmail.com>
2023-09-12 16:14:21 +0100

    added cloud postgres db url

--- app/database.py
+++ app/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:adminadmin@localhost/person-resource"
+#SQLALCHEMY_DATABASE_URL = "postgresql://postgres:adminadmin@localhost/person-resource"
+SQLALCHEMY_DATABASE_URL = "postgresql://resource_db_instance_user:lEEp3bH46uYJ0Tlsn3PdsAhuN8qjFxHi@dpg-ck07ado21fec73dghss0-a.oregon-postgres.render.com/resource_db_instance"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
